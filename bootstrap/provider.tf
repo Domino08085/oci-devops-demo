83192e80af9caa75c5b551577747af3416a2a05a
domino08085 <domino08085@o2.pl>
2025-10-10 01:27:55 +0200

    update: add environment and update files

--- bootstrap/provider.tf
+++ bootstrap/provider.tf
@@ -7,11 +7,11 @@ terraform {
   }
 }
 provider "oci" {
-  config_file_profile = "DEFAULT"
-  # region           = var.region
-  # tenancy_ocid     = var.tenancy_ocid
-  # user_ocid        = var.user_ocid
-  # fingerprint      = var.fingerprint
-  # private_key      = var.private_key_password
+  #config_file_profile = "DEFAULT"
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key      = var.private_key_pem
   # private_key_password = var.private_key_password
 }
