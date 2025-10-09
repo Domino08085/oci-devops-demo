3d80f681f35ff2913e1258705d67ac94b7a7b767
domino08085 <domino08085@o2.pl>
2025-10-10 01:15:34 +0200

    feat: update bootstrap workflow

--- bootstrap/provider.tf
+++ bootstrap/provider.tf
@@ -1,14 +1,17 @@
 terraform {
   required_version = ">= 1.4.0"
   required_providers {
-    oci = { source = "oracle/oci", version = ">=5.0.0" }
+    oci = { 
+      source = "oracle/oci"
+      version = ">=5.0.0" }
   }
 }
 provider "oci" {
-  region           = var.region
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key      = var.private_key_password 
+  config_file_profile = "DEFAULT"
+  # region           = var.region
+  # tenancy_ocid     = var.tenancy_ocid
+  # user_ocid        = var.user_ocid
+  # fingerprint      = var.fingerprint
+  # private_key      = var.private_key_password
   # private_key_password = var.private_key_password
 }
