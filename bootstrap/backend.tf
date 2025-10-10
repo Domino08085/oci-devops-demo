fd9e57f1bd833417e7788c21296f2c1395a277c1
domino08085 <domino08085@o2.pl>
2025-10-10 03:21:11 +0200

    test

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -4,12 +4,12 @@ terraform {
     key                         = "bootstrap/terraform.tfstate"
     region                      = "us-east-1"
 
-    endpoints = {
-      s3 = "https://objectstorage.eu-frankfurt-1.oraclecloud.com"
-    }
+    # endpoints = {
+    #   s3 = "https://objectstorage.eu-frankfurt-1.oraclecloud.com"
+    # }
 
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    force_path_style            = true
+    # skip_credentials_validation = true
+    # skip_requesting_account_id  = true
+    # force_path_style            = true
   }
 }
