6752dc80016857224d3ab9ec35e481da2c454677
domino08085 <domino08085@o2.pl>
2025-10-10 02:59:24 +0200

    fix: backend migration

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -2,8 +2,12 @@ terraform {
   backend "s3" {
     bucket                      = "tfstate-bucket"    
     key                         = "bootstrap/terraform.tfstate"
-    region                      = "eu-frankfurt-1"
-    endpoint                    = "https://objectstorage.eu-frankfurt-1.oraclecloud.com"
+    region                      = "us-east-1"
+
+    endpoints = {
+      s3 = "https://objectstorage.eu-frankfurt-1.oraclecloud.com"
+    }
+
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     force_path_style            = true
