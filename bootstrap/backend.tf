f9deed3b5e8288b4d53cb9c3b902f13974f6edd1
domino08085 <domino08085@o2.pl>
2025-10-10 02:48:12 +0200

    update: add backend for bootstrap

---
+++ bootstrap/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket                      = var.tfstate_bucket_name      
+    key                         = "bootstrap/terraform.tfstate"
+    region                      = "eu-frankfurt-1"
+    endpoint                    = "https://objectstorage.eu-frankfurt-1.oraclecloud.com"
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
+  }
+}
