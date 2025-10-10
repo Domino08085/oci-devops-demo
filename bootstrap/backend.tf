7af8fceb1b0f6399be7bcef71ff2383bfccb573e
domino08085 <domino08085@o2.pl>
2025-10-10 03:46:06 +0200

    fix: backend bootstrap

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "oci" {
     bucket         = "tfstate-bucket"
     namespace      = "frxdvqsyd4jy"
-    compartment_id = "ocid1.compartment.oc1..aaaaaaaahjpyn7ffagxkqpybbrckq7ysmyir65mmybxk7zkpkb6cevjuwm4q"
     region         = "eu-frankfurt-1"
   }
 }
