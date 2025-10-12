41b3d0d5de8801034b5ca64c3e7a9c04d3226f3f
domino08085 <domino08085@o2.pl>
2025-10-12 23:02:39 +0200

    fix: oci DEFAULT config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,9 +9,10 @@ terraform {
 }
 
 provider "oci" {
-  region       = var.region
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid    = var.user_ocid
-  fingerprint  = var.fingerprint
-  private_key_path = var.private_key_path
+  config_file_profile = "DEFAULT"
+  # region       = var.region
+  # tenancy_ocid = var.tenancy_ocid
+  # user_ocid    = var.user_ocid
+  # fingerprint  = var.fingerprint
+  # private_key_path = var.private_key_path
 }
