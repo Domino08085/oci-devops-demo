152433832a01663e43b6c46acda5fa85266c0b3c
domino08085 <domino08085@o2.pl>
2025-10-12 22:55:27 +0200

    fix: varaibles tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,8 @@ variable "tenancy_ocid" {}
 variable "compartment_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
+variable "private_key_pem" { sensitive = true }
+
 
 variable "oke_kubernetes_version" {
   default = "v1.34.1"
