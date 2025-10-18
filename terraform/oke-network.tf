8d44e3fbcb7c8e910f593f12ae65acb8bafc8f59
domino08085 <domino08085@o2.pl>
2025-10-18 09:53:22 +0200

    fix: network rules

--- terraform/oke-network.tf
+++ terraform/oke-network.tf
@@ -12,10 +12,10 @@ resource "oci_core_subnet" "oke_k8s_endpoint_subnet" {
   cidr_block          = lookup(var.network_cidrs, "ENDPOINT-SUBNET-REGIONAL-CIDR")
   display_name        = "oke-k8s-endpoint-subnet"
   dns_label           = "okek8sn"
-  prohibit_public_ip_on_vnic = true
+  prohibit_public_ip_on_vnic = false
   route_table_id              = oci_core_route_table.oke_public_route_table.id
   dhcp_options_id            =  oci_core_vcn.vcn.default_dhcp_options_id
-  security_list_ids           = [oci_core_security_list.oke_nodes_security_list.id]
+  security_list_ids           = [oci_core_security_list.oke_endpoint_security_list.id]
 }
 
 resource "oci_core_subnet" "oke_nodes_subnet" {
@@ -25,7 +25,7 @@ resource "oci_core_subnet" "oke_nodes_subnet" {
   dns_label                  = "okenodesn${random_string.deploy_id.result}"
   vcn_id                     = oci_core_vcn.vcn.id
   prohibit_public_ip_on_vnic = true
-  route_table_id             = oci_core_route_table.oke_public_route_table.id
+  route_table_id             = oci_core_route_table.oke_private_route_table.id
   dhcp_options_id            = oci_core_vcn.vcn.default_dhcp_options_id
   security_list_ids          = [oci_core_security_list.oke_nodes_security_list.id]
 }
@@ -52,6 +52,28 @@ resource "oci_core_route_table" "oke_public_route_table" {
     destination       = lookup(var.network_cidrs, "ALL-CIDR")
     destination_type  = "CIDR_BLOCK"
     network_entity_id = oci_core_internet_gateway.oke_internet_gateway.id
+  }
+}
+
+resource "oci_core_route_table" "oke_private_route_table" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "oke-private-route-table-${random_string.deploy_id.result}"
+
+  # default to Internet via NAT
+  route_rules {
+    description       = "Traffic to the internet"
+    destination       = lookup(var.network_cidrs, "ALL-CIDR")
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_nat_gateway.oke_nat_gateway.id
+  }
+
+  # traffic to Oracle Services Network via Service Gateway
+  route_rules {
+    description       = "Traffic to OCI services"
+    destination       = lookup(data.oci_core_services.all_services.services[0], "cidr_block")
+    destination_type  = "SERVICE_CIDR_BLOCK"
+    network_entity_id = oci_core_service_gateway.oke_service_gateway[0].id
   }
 }
 
